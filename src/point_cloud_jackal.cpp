15236a3ff35e3d21cc8d41eeed8a218f20cc818c
mzwang <wang.mao@husky.neu.edu>
2018-02-14 16:46:39 -0500

    add point cloud filter

--- src/point_cloud_jackal.cpp
+++ src/point_cloud_jackal.cpp
@@ -11,6 +11,7 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl_ros/point_cloud.h>
@@ -23,9 +24,11 @@
 class PointCloudProc
 {
   public:
-    PointCloudProc() : cloud_transformed_(new pcl::PointCloud<pcl::PointXYZ>),
+    PointCloudProc() : cloud_transformed_(new pcl::PointCloud<pcl::PointXYZ>), cloud_filtered_(new pcl::PointCloud<pcl::PointXYZ>),
                        cloud_hull(new pcl::PointCloud<pcl::PointXYZ>), cloud_raw_(new pcl::PointCloud<pcl::PointXYZ>)
     {
+        filter_range_ = 1.0;
+
         planar_segment_src_ = nh_.advertiseService("planer_segment", &PointCloudProc::planarSegmentationCB, this);
         pc_sub_ = nh_.subscribe("/kinect2/qhd/points", 1, &PointCloudProc::pointcloudcb, this);
         point_cloud_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ>>("segmented_plane_point_cloud", 1000);
@@ -46,6 +49,32 @@ class PointCloudProc
         return transform_success;
     }
 
+    bool filterPointCloud(geometry_msgs::Point center)
+    {
+        pcl::PassThrough<pcl::PointXYZ> pass;
+        pass.setInputCloud(cloud_transformed_);
+        pass.setFilterFieldName("x");
+        pass.setFilterLimits(center.x - filter_range_, center.x + filter_range_);
+        pass.filter(*cloud_filtered_);
+        pass.setInputCloud(cloud_filtered_);
+        pass.setFilterFieldName("y");
+        pass.setFilterLimits(center.y - filter_range_, center.y + filter_range_);
+        pass.filter(*cloud_filtered_);
+        pass.setInputCloud(cloud_filtered_);
+        pass.setFilterFieldName("z");
+        pass.setFilterLimits(center.z - filter_range_, center.z + filter_range_);
+        pass.filter(*cloud_filtered_);
+
+        ROS_INFO("Point cloud is filtered!");
+        if (cloud_filtered_->points.size() == 0)
+        {
+            ROS_WARN("Point cloud is empty after filtering!");
+            return false;
+        }
+
+        return true;
+    }
+
     bool planarSegmentation(bool create_srv_res)
     {
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>);
@@ -58,12 +87,12 @@ class PointCloudProc
         seg.setOptimizeCoefficients(true);
         // Mandatory set plane to be parallel to Z axis within a 15 degrees tolerance
         seg.setModelType(pcl::SACMODEL_PARALLEL_PLANE);
-        seg.setMaxIterations(500);   // iteration limits decides segmentation goodness
+        seg.setMaxIterations(500); // iteration limits decides segmentation goodness
         seg.setMethodType(pcl::SAC_RANSAC);
         seg.setAxis(axis);
         seg.setEpsAngle(pcl::deg2rad(15.0f));
         seg.setDistanceThreshold(0.01);
-        seg.setInputCloud(cloud_transformed_);
+        seg.setInputCloud(cloud_filtered_);
         seg.segment(*inliers, *coefficients);
 
         if (inliers->indices.size() == 0)
@@ -73,14 +102,14 @@ class PointCloudProc
         }
 
         // extract inline points original point coulds
-        extract_.setInputCloud(cloud_transformed_);
+        extract_.setInputCloud(cloud_filtered_);
         extract_.setNegative(false);
         extract_.setIndices(inliers);
         extract_.filter(*cloud_plane);
         ROS_INFO_STREAM("# of points in plane: " << cloud_plane->points.size());
 
         // publish segmented point cloud
-        point_cloud_pub_.publish(cloud_plane);
+        point_cloud_pub_.publish(cloud_filtered_);
 
         // Create a Convex Hull representation of the plane
         chull.setInputCloud(cloud_plane);
@@ -156,7 +185,14 @@ class PointCloudProc
             res.success = false;
             return true;
         }
-        
+
+        if (!this->filterPointCloud(req.center))
+        {
+            ROS_INFO("failed filter point cloud");
+            res.success = false;
+            return true;
+        }
+
         ROS_INFO("stage 1");
         if (this->planarSegmentation(true))
         {
@@ -183,10 +219,11 @@ class PointCloudProc
 
     point_cloud_jackal::Plane plane_object_;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_raw_, cloud_transformed_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_raw_, cloud_transformed_, cloud_filtered_;
     pcl::ExtractIndices<pcl::PointXYZ> extract_;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull;
     pcl::ConvexHull<pcl::PointXYZ> chull;
+    float filter_range_;
 };
 
 int main(int argc, char **argv)
